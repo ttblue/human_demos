0196f71aa30bb8f1784f9b2b9daa377eb99d9f17
Ankush Gupta <ankush.me@hotmail.com>
2013-08-18 05:58:08 -0700

    Calibration doesn't work on real data. =/

--- hd_test/calib_hydra_kinect.py
+++ hd_test/calib_hydra_kinect.py
@@ -3,11 +3,14 @@ roslib.load_manifest('calib_hydra_kinect')
 import rospy
 import tf; roslib.load_manifest('tf')
 
+
 import argparse
-import numpy as np
+import numpy as np, scipy as scp
 from colorize import colorize
 
+from rapprentice import conversions
 
+np.set_printoptions(precision=5, suppress=True)
 
 def avg_quaternions(qs):
     """
@@ -40,6 +43,8 @@ def get_transforms(marker, hydra, n_tfm , n_avg):
 
     tf_sub = tf.TransformListener()
 
+    I_0 = np.eye(4)
+    I_0[3,3] = 0
     
     ar_tfms    = []
     hydra_tfms = []
@@ -61,28 +66,215 @@ def get_transforms(marker, hydra, n_tfm , n_avg):
             while htrans == None:
                 #now = rospy.Time.now()
                 #tf_sub.waitForTransform(camera_frame, marker_frame, now)
+#                 mtrans, mrot = tf_sub.lookupTransform(marker_frame, camera_frame, rospy.Time(0))
+#                 (htrans,hrot) = tf_sub.lookupTransform(paddle_frame, hydra_frame, rospy.Time(0))
                 mtrans, mrot = tf_sub.lookupTransform(camera_frame, marker_frame, rospy.Time(0))
-                (htrans,hrot) = tf_sub.lookupTransform(hydra_frame, paddle_frame, rospy.Time(0))
+                htrans, hrot = tf_sub.lookupTransform(hydra_frame, paddle_frame, rospy.Time(0))
                 #try:
                 #    tf_sub.waitForTransform(hydra_frame, paddle_frame, now, rospy.Duration(0.05))
                 #except (tf.LookupException):
                 #    continue
-    
+
             m_ts = np.r_[m_ts, np.array(mtrans, ndmin=2)]
             h_ts = np.r_[h_ts, np.array(htrans, ndmin=2)]
             m_qs = np.r_[m_qs, np.array(mrot, ndmin=2)]
             h_qs = np.r_[h_qs, np.array(hrot, ndmin=2)]
-        
+          
         mtrans_avg = np.sum(m_ts, axis=0) / n_avg
         mrot_avg   = avg_quaternions(m_qs)
         htrans_avg = np.sum(h_ts, axis=0) / n_avg
         hrot_avg   = avg_quaternions(h_qs)
+          
+#         ar_tfm = tf_sub.fromTranslationRotation(mtrans_avg, mrot_avg)
+#         h_tfm = tf_sub.fromTranslationRotation(htrans_avg, hrot_avg)
+        ar_tfm = conversions.trans_rot_to_hmat(mtrans_avg,mrot_avg)
+        h_tfm = conversions.trans_rot_to_hmat(htrans_avg,hrot_avg)
+         
+        print "\nar:"
+        print ar_tfm
+        print ar_tfm.dot(I_0).dot(ar_tfm.T)
+        print "h:"
+        print h_tfm
+        print h_tfm.dot(I_0).dot(h_tfm.T), "\n"
+         
+#         ar_tfm = conversions.trans_rot_to_hmat(mtrans,mrot)
+#         h_tfm = conversions.trans_rot_to_hmat(htrans,hrot)
+#         print "\nar:"
+#         print ar_tfm
+#         print ar_tfm.dot(I_0).dot(ar_tfm.T)
+#         print "h:"
+#         print h_tfm
+#         print h_tfm.dot(I_0).dot(h_tfm.T), "\n"
         
-        ar_tfms.append(tf_sub.fromTranslationRotation(mtrans_avg, mrot_avg))
-        hydra_tfms.append(tf_sub.fromTranslationRotation(htrans_avg, hrot_avg))
+        ar_tfms.append(ar_tfm)
+        hydra_tfms.append(h_tfm)
         
     return (ar_tfms, hydra_tfms)
 
+def get_hydra_transforms(n_tfm , n_avg):
+    """
+    Returns a tuple of two list of N_TFM transforms, each
+    the average of N_AVG transforms
+    corresponding to the AR marker with ID = MARKER and 
+    the hydra paddle of the HYDRA ('right' or 'left') side.
+    """
+
+    hydra_frame  = 'hydra_base'
+    paddle1_frame = 'hydra_right_pivot'
+    paddle2_frame = 'hydra_left_pivot'
+
+    tf_sub = tf.TransformListener()
+
+    I_0 = np.eye(4)
+    I_0[3,3] = 0
+    
+    ar_tfms    = []
+    hydra_tfms = []
+    
+    
+    for i in xrange(n_tfm+1):
+        raw_input(colorize("Transform %d of %d : Press return when ready to capture transforms"%(i, n_tfm), "red", True))
+        
+        ## transforms which need to be averaged.
+        m_ts = np.empty((0, 3))
+        m_qs = np.empty((0,4))
+        h_ts = np.empty((0, 3))
+        h_qs = np.empty((0,4))
+        
+        for j in xrange(n_avg):            
+            print colorize('\tGetting averaging transform : %d of %d ...'%(j,n_avg-1), "blue", True)
+            
+            mtrans, mrot, htrans, hrot = None, None, None, None
+            while htrans == None:
+                #now = rospy.Time.now()
+                #tf_sub.waitForTransform(camera_frame, marker_frame, now)
+#                 mtrans, mrot = tf_sub.lookupTransform(marker_frame, camera_frame, rospy.Time(0))
+#                 (htrans,hrot) = tf_sub.lookupTransform(paddle_frame, hydra_frame, rospy.Time(0))
+                mtrans, mrot = tf_sub.lookupTransform(hydra_frame, paddle1_frame, rospy.Time(0))
+                htrans, hrot = tf_sub.lookupTransform(hydra_frame, paddle2_frame, rospy.Time(0))
+                #try:
+                #    tf_sub.waitForTransform(hydra_frame, paddle_frame, now, rospy.Duration(0.05))
+                #except (tf.LookupException):
+                #    continue
+
+            m_ts = np.r_[m_ts, np.array(mtrans, ndmin=2)]
+            h_ts = np.r_[h_ts, np.array(htrans, ndmin=2)]
+            m_qs = np.r_[m_qs, np.array(mrot, ndmin=2)]
+            h_qs = np.r_[h_qs, np.array(hrot, ndmin=2)]
+          
+        mtrans_avg = np.sum(m_ts, axis=0) / n_avg
+        mrot_avg   = avg_quaternions(m_qs)
+        htrans_avg = np.sum(h_ts, axis=0) / n_avg
+        hrot_avg   = avg_quaternions(h_qs)
+          
+#         ar_tfm = tf_sub.fromTranslationRotation(mtrans_avg, mrot_avg)
+#         h_tfm = tf_sub.fromTranslationRotation(htrans_avg, hrot_avg)
+        ar_tfm = conversions.trans_rot_to_hmat(mtrans_avg,mrot_avg)
+        h_tfm = conversions.trans_rot_to_hmat(htrans_avg,hrot_avg)
+         
+        print "\nar:"
+        print ar_tfm
+        print ar_tfm.dot(I_0).dot(ar_tfm.T)
+        print "h:"
+        print h_tfm
+        print h_tfm.dot(I_0).dot(h_tfm.T), "\n"
+         
+#         ar_tfm = conversions.trans_rot_to_hmat(mtrans,mrot)
+#         h_tfm = conversions.trans_rot_to_hmat(htrans,hrot)
+#         print "\nar:"
+#         print ar_tfm
+#         print ar_tfm.dot(I_0).dot(ar_tfm.T)
+#         print "h:"
+#         print h_tfm
+#         print h_tfm.dot(I_0).dot(h_tfm.T), "\n"
+        
+        ar_tfms.append(ar_tfm)
+        hydra_tfms.append(h_tfm)
+        
+    return (ar_tfms, hydra_tfms)
+
+
+def get_ar_transforms(marker1, marker2, n_tfm , n_avg):
+    """
+    Returns a tuple of two list of N_TFM transforms, each
+    the average of N_AVG transforms
+    corresponding to the AR marker with ID = MARKER and 
+    the hydra paddle of the HYDRA ('right' or 'left') side.
+    """
+
+    camera_frame = 'camera_link'
+    marker1_frame = 'ar_marker_%d'%marker1
+    marker2_frame = 'ar_marker_%d'%marker2    
+
+    tf_sub = tf.TransformListener()
+
+    I_0 = np.eye(4)
+    I_0[3,3] = 0
+    
+    ar_tfms    = []
+    hydra_tfms = []
+    
+    
+    for i in xrange(n_tfm+1):
+        raw_input(colorize("Transform %d of %d : Press return when ready to capture transforms"%(i, n_tfm), "red", True))
+        
+        ## transforms which need to be averaged.
+        m_ts = np.empty((0, 3))
+        m_qs = np.empty((0,4))
+        h_ts = np.empty((0, 3))
+        h_qs = np.empty((0,4))
+        
+        for j in xrange(n_avg):            
+            print colorize('\tGetting averaging transform : %d of %d ...'%(j,n_avg-1), "blue", True)
+            
+            mtrans, mrot, htrans, hrot = None, None, None, None
+            while htrans == None:
+                #now = rospy.Time.now()
+                #tf_sub.waitForTransform(camera_frame, marker_frame, now)
+#                 mtrans, mrot = tf_sub.lookupTransform(marker_frame, camera_frame, rospy.Time(0))
+#                 (htrans,hrot) = tf_sub.lookupTransform(paddle_frame, hydra_frame, rospy.Time(0))
+                mtrans, mrot = tf_sub.lookupTransform(camera_frame, marker1_frame, rospy.Time(0))
+                htrans, hrot = tf_sub.lookupTransform(camera_frame, marker2_frame, rospy.Time(0))
+                #try:
+                #    tf_sub.waitForTransform(hydra_frame, paddle_frame, now, rospy.Duration(0.05))
+                #except (tf.LookupException):
+                #    continue
+
+            m_ts = np.r_[m_ts, np.array(mtrans, ndmin=2)]
+            h_ts = np.r_[h_ts, np.array(htrans, ndmin=2)]
+            m_qs = np.r_[m_qs, np.array(mrot, ndmin=2)]
+            h_qs = np.r_[h_qs, np.array(hrot, ndmin=2)]
+          
+        mtrans_avg = np.sum(m_ts, axis=0) / n_avg
+        mrot_avg   = avg_quaternions(m_qs)
+        htrans_avg = np.sum(h_ts, axis=0) / n_avg
+        hrot_avg   = avg_quaternions(h_qs)
+          
+#         ar_tfm = tf_sub.fromTranslationRotation(mtrans_avg, mrot_avg)
+#         h_tfm = tf_sub.fromTranslationRotation(htrans_avg, hrot_avg)
+        ar_tfm = conversions.trans_rot_to_hmat(mtrans_avg,mrot_avg)
+        h_tfm = conversions.trans_rot_to_hmat(htrans_avg,hrot_avg)
+         
+        print "\nar:"
+        print ar_tfm
+        print ar_tfm.dot(I_0).dot(ar_tfm.T)
+        print "h:"
+        print h_tfm
+        print h_tfm.dot(I_0).dot(h_tfm.T), "\n"
+         
+#         ar_tfm = conversions.trans_rot_to_hmat(mtrans,mrot)
+#         h_tfm = conversions.trans_rot_to_hmat(htrans,hrot)
+#         print "\nar:"
+#         print ar_tfm
+#         print ar_tfm.dot(I_0).dot(ar_tfm.T)
+#         print "h:"
+#         print h_tfm
+#         print h_tfm.dot(I_0).dot(h_tfm.T), "\n"
+        
+        ar_tfms.append(ar_tfm)
+        hydra_tfms.append(h_tfm)
+        
+    return (ar_tfms, hydra_tfms)
 
 
 def solve_sylvester (tfms1, tfms2):
@@ -100,7 +292,7 @@ def solve_sylvester (tfms1, tfms2):
     s2_t0_inv = np.linalg.inv(tfms2[0])
     
     for i in range(1,len(tfms1)):
-        M = np.kron(I, s1_t0_inv.dot(tfms1[i])) - np.kron(s2_t0_inv.dot(tfms2[i]),I)
+        M = np.kron(I, s1_t0_inv.dot(tfms1[i])) - np.kron(s2_t0_inv.dot(tfms2[i]).T,I)
         M_final = np.r_[M_final, M]
     
     # add the constraints on the last row of the transformation matrix to be == [0,0,0,1]
@@ -114,6 +306,89 @@ def solve_sylvester (tfms1, tfms2):
     X = np.linalg.lstsq(M_final,L_final)[0]
     print M_final.dot(X)
     tt = np.reshape(X,(4,4),order='F')
+    return tt
+
+def solve_sylvester2 (tfms1, tfms2):
+    """
+    Solves the system of Sylvester's equations to find the calibration transform.
+    Returns the calibration transform from sensor 1 (corresponding to tfms1) to sensor 2.
+    This functions forces the bottom row to be 0,0,0,1 by neglecting columns of M and changing L.
+    """
+
+    assert len(tfms1) == len(tfms2) and len(tfms1) >= 2
+    I = np.eye(4)
+    I_0 = np.copy(I)
+    I_0[3,3] = 0
+        
+    M_final = np.empty((0,16))
+
+    s1_t0_inv = np.linalg.inv(tfms1[0])
+    s2_t0_inv = np.linalg.inv(tfms2[0])
+    
+    print "\n CONSTRUCTING M: \n"
+    
+    for i in range(1,len(tfms1)):
+        del1 = s1_t0_inv.dot(tfms1[i])
+        del2 = s2_t0_inv.dot(tfms2[i])
+        
+        print "\n del1:"
+        print del1
+        print del1.dot(I_0).dot(del1.T)
+        print "\n del2:"
+        print del2, '\n'
+        print del2.dot(I_0).dot(del2.T)
+        
+        M = np.kron(I, del1) - np.kron(del2.T,I)
+        M_final = np.r_[M_final, M]
+    
+    L_final = -1*np.copy(M_final[:,15])
+    M_final = scp.delete(M_final, (3,7,11,15), 1) 
+
+    X = np.linalg.lstsq(M_final,L_final)[0]
+    print M_final.dot(X) - L_final
+    
+    X2 = (np.reshape(scp.delete(np.eye(4),3,0),12,order="F"))
+    print M_final.dot(X2) - L_final
+    
+    tt = np.reshape(X,(3,4),order='F')
+    tt = np.r_[tt,np.array([[0,0,0,1]])]
+    
+    print tt.T.dot(tt)
+    
+    return tt
+    
+def solve_sylvester3 (tfms1, tfms2):
+    """
+    Solves the system of Sylvester's equations to find the calibration transform.
+    Returns the calibration transform from sensor 1 (corresponding to tfms1) to sensor 2.
+    This functions forces the bottom row to be 0,0,0,1 by neglecting columns of M and changing L.
+    Delta transfrom from previous iteration.
+    """
+
+    assert len(tfms1) == len(tfms2) and len(tfms1) >= 2
+    I = np.eye(4)
+        
+    M_final = np.empty((0,16))
+    
+    print "\n CONSTRUCTING M: \n"
+    
+    for i in range(1,len(tfms1)):
+        s1_inv = np.linalg.inv(tfms1[i-1])
+        s2_inv = np.linalg.inv(tfms2[i-1])
+        M = np.kron(I, s1_inv.dot(tfms1[i])) - np.kron(s2_inv.dot(tfms2[i]).T,I)
+        M_final = np.r_[M_final, M]
+    
+    L_final = -1*np.copy(M_final)[:,15]
+    M_final = scp.delete(M_final, (3,7,11,15), 1) 
+
+    X = np.linalg.lstsq(M_final,L_final)[0]
+    print M_final.dot(X) - L_final
+    
+    tt = np.reshape(X,(3,4),order='F')
+    tt = np.r_[tt,np.array([[0,0,0,1]])]
+    
+    print tt.T.dot(tt)
+    
     return tt
 
 
@@ -141,12 +416,21 @@ if __name__ == '__main__':
     parser.add_argument('--publish_tf', help="whether to publish the transform between hydra_base and camera_link", default=True)
     vals = parser.parse_args()
 
-    ar_tfms, hydra_tfms = get_transforms(vals.marker, vals.hydra, vals.n_tfm, vals.n_avg)
+    #ar_tfms, hydra_tfms = get_transforms(vals.marker, vals.hydra, vals.n_tfm, vals.n_avg)
+    ar_tfms, hydra_tfms = get_hydra_transforms(vals.n_tfm, vals.n_avg)
+    #ar_tfms, hydra_tfms = get_ar_transforms(8,5, vals.n_tfm, vals.n_avg)
     #print "AR Transforms: ", ar_tfms
     #print "Hydra Transforms: ", hydra_tfms
     
+    #import cPickle as pickle
+    #pickle.dump({"AR":ar_tfms, "Hydra":hydra_tfms}, open( "transforms.p", "wb" ))
+    
     if vals.publish_tf:
-        T_ms = solve_sylvester(ar_tfms, hydra_tfms)
+        T_ms = solve_sylvester2(ar_tfms, hydra_tfms)
         T_ch = ar_tfms[0].dot(T_ms).dot(np.linalg.inv(hydra_tfms[0]))
+        print T_ch
+        
+#        print T_ch.dot(hydra_tfms[0].dot(np.linalg.inv(T_ms))).dot(np.linalg.inv(ar_tfms[0]))
+        
         publish_tf(T_ch, 'camera_link', 'hydra_base')                                
         #publish_tf(, 'ar_marker_%d'%vals.marker, 'hydra_%s_pivot'%vals.hydra)
--- hd_test/test_tfm.py
+++ hd_test/test_tfm.py
@@ -1,4 +1,7 @@
-import numpy as np
+import numpy as np, numpy.linalg as nlg
+import scipy as scp
+
+np.set_printoptions(precision=5, suppress=True)
 
 def rotation_matrix(axis,theta):
     axis = axis/np.sqrt(np.dot(axis,axis))
@@ -9,18 +12,18 @@ def rotation_matrix(axis,theta):
                      [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
     
 
-def make_obs():
+def make_obs(trans_SCALE=0.1, theta_SCALE=1.5):
     axis = np.random.randn(3)
-    theta = np.random.rand()*np.pi*2
+    theta = np.random.rand()*np.pi*theta_SCALE
     
-    SCALE = 2
-    t = np.random.randn(3)*SCALE
+    t = np.random.randn(3)*trans_SCALE
     
     tfm = np.eye(4)
     tfm[0:3,0:3] = rotation_matrix(axis, theta)
     tfm[0:3,3] = t
     
     return tfm
+
 
 #Observations
 def test_tfm (n):
@@ -63,3 +66,280 @@ def test_tfm (n):
     
     assert(np.allclose(M_final.dot(X2),L_final, atol=0.001))
     assert (np.allclose(Tfm,Tms, atol=0.001))
+    
+# Change M after assuming last row of transform is [0,0,0,1]
+def test_tfm2 (n, addnoise=False):
+    
+    Tms = make_obs()
+    Tsm = np.linalg.inv(Tms)
+    I = np.eye(4)
+    
+    M_final = np.empty((0,16))
+    
+    for i in range(n):
+        del_Ts = make_obs(0.05, 0.05)
+        if addnoise:
+            noise = make_obs(0.01,0.01)
+            del_Tm = Tms.dot(del_Ts.dot(Tsm)).dot(noise)
+        else:
+            del_Tm = Tms.dot(del_Ts.dot(Tsm))
+        print "Observation %i"%(i+1)
+        print "Delta Ts:"
+        print del_Ts
+        print "Delta Tm:"
+        print del_Tm, '\n'
+        
+        M = np.kron(I,del_Tm)-np.kron(del_Ts.T,I)
+        
+        M_final = np.r_[M_final, M]
+    
+    
+    L_final = -1*M_final[:,15]
+    M_final = scp.delete(M_final, (3,7,11,15), 1) 
+
+            
+    X = np.linalg.lstsq(M_final,L_final)[0]
+    Tfm = np.reshape(X,(3,4),order='F')
+    print Tfm.shape
+    Tfm = np.r_[Tfm,np.array([[0,0,0,1]])]
+
+    np.set_printoptions(precision=5)    
+    
+    print Tfm
+    print Tms
+    
+    R = Tfm[0:3,0:3]
+    print R.T.dot(R)
+    
+    X2 = scp.delete(np.reshape(Tms,16,order="F"),(3,7,11,15),0)
+    print X2
+    
+    if not addnoise:
+        assert(np.allclose(M_final.dot(X2),L_final, atol=0.001))
+        assert (np.allclose(Tfm,Tms, atol=0.001))
+    
+def solve_sylvester2 (tfms1, tfms2, addnoise=True):
+    """
+    Solves the system of Sylvester's equations to find the calibration transform.
+    Returns the calibration transform from sensor 1 (corresponding to tfms1) to sensor 2.
+    This functions forces the bottom row to be 0,0,0,1 by neglecting columns of M and changing L.
+    """
+
+    assert len(tfms1) == len(tfms2) and len(tfms1) >= 2
+    I = np.eye(4)
+        
+    M_final = np.empty((0,16))
+
+    s1_t0_inv = np.linalg.inv(tfms1[0])
+    s2_t0_inv = np.linalg.inv(tfms2[0])
+    
+    print "\n CONSTRUCTING M: \n"
+    
+    for i in range(1,len(tfms1)):
+        if addnoise:
+            noise = make_obs(0.01,0.01)
+        else:
+            noise = np.eye(4)
+        M = np.kron(I, s1_t0_inv.dot(tfms1[i])) - np.kron(s2_t0_inv.dot(tfms2[i]).dot(noise).T,I)
+        M_final = np.r_[M_final, M]
+    
+    # add the constraints on the last row of the transformation matrix to be == [0,0,0,1]
+    L_final = -1*np.copy(M_final)[:,15]
+    M_final = scp.delete(M_final, (3,7,11,15), 1) 
+
+    X = np.linalg.lstsq(M_final,L_final)[0]
+    print M_final.dot(X) - L_final
+    tt = np.reshape(X,(3,4),order='F')
+    tt = np.r_[tt,np.array([[0,0,0,1]])]
+    
+    print tt.T.dot(tt)
+    
+    return tt
+    
+def test_tfm3 (n):
+    """
+    Test of the full pipeline.
+    """
+    Tch = make_obs(0.5)
+    Thc = nlg.inv(Tch)
+    Tms = make_obs(0.1)
+    I_0 = np.eye(4)
+    I_0[3,3] = 0
+    
+    print "Tch:"
+    print Tch
+    print Tch.dot(I_0).dot(Tch.T)
+    print "Tms:"
+    print Tms
+    print Tms.dot(I_0).dot(Tms.T), '\n'
+    
+    
+    tfms1 = []
+    tfms2 = []
+    for i in range(n):
+        Tcm = make_obs(0.3)
+        Ths = Thc.dot(Tcm).dot(Tms)
+        
+        tfms1.append(Tcm)
+        tfms2.append(Ths)
+        print "Observation %i"%(i+1)
+        print "Tcm:"
+        print Tcm
+        print Tcm.dot(I_0).dot(Tcm.T)
+        print "Ths:"
+        print Ths
+        print Ths.dot(I_0).dot(Ths.T)
+        print Tcm.dot(Tms).dot(nlg.inv(Ths)).dot(Thc), '\n'
+        
+    Tms_calib = solve_sylvester2(tfms1, tfms2)
+    
+    print "Tms: \n", Tms
+    print "Tms_calib: \n", Tms_calib
+    
+    R = Tms_calib[0:3,0:3]
+    print R.T.dot(R)
+    
+    assert (np.allclose(Tms_calib,Tms, atol=0.001))
+    
+def test_tfm4 (n):
+    """
+    Test of the full pipeline.
+    """
+    Tch = np.eye(4)#make_obs(0.5)
+    Thc = nlg.inv(Tch)
+    Tms = make_obs(0.1)
+    I_0 = np.eye(4)
+    I_0[3,3] = 0
+    
+    print "Tch:"
+    print Tch
+    print Tch.dot(I_0).dot(Tch.T)
+    print "Tms:"
+    print Tms
+    print Tms.dot(I_0).dot(Tms.T), '\n'
+    
+    
+    tfms1 = []
+    tfms2 = []
+    for i in range(n):
+        Tcm = make_obs(0.3)
+        noise = make_obs(0.01,0.02)
+        Ths = Thc.dot(Tcm).dot(Tms).dot(noise)
+        
+        tfms1.append(Tcm)
+        tfms2.append(Ths)
+        print "Observation %i"%(i+1)
+        print "Tcm:"
+        print Tcm
+        print Tcm.dot(I_0).dot(Tcm.T)
+        print "Ths:"
+        print Ths
+        print Ths.dot(I_0).dot(Ths.T)
+        print Tcm.dot(Tms).dot(nlg.inv(Ths)).dot(Thc), '\n'
+        
+    Tms_calib = solve_sylvester2(tfms1, tfms2)
+    
+    print "Tms: \n", Tms
+    print "Tms_calib: \n", Tms_calib
+    
+    print "Tch: \n", Tch
+    print "Tch calib: \n", tfms1[0].dot(Tms).dot(np.linalg.inv(tfms2[0]))
+    
+    R = Tms_calib[0:3,0:3]
+    print R.T.dot(R)
+    
+    #assert (np.allclose(Tms_calib,Tms, atol=0.001))
+
+def test_tfm_val1():
+
+    Tch = make_obs(0.5)
+    Thc = nlg.inv(Tch)
+    Tms = make_obs(0.1)
+    I_0 = np.eye(4)
+    I_0[3,3] = 0
+
+    tfms1 = []
+    tfms2 = []
+    
+    tfms1.append([[-0.0096,  -0.03728, -0.99926,  0.75043],
+                  [ 0.99995,  0.,      -0.0096,  -0.07002],
+                  [ 0.00036, -0.9993,   0.03728, -0.02286],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    tfms2.append(Thc.dot(tfms1[0]).dot(Tms))
+    
+    tfms1.append([[-0.0273,  -0.70346, -0.71021,  0.75183],
+                  [ 0.99952, -0.00897, -0.02954, -0.09279],
+                  [ 0.0144,  -0.71068,  0.70337, -0.088  ],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    tfms2.append(Thc.dot(tfms1[1]).dot(Tms))
+
+    tfms1.append([[-0.48571, -0.1099,  -0.86718,  0.65788],
+                  [ 0.82301,  0.27675, -0.49605,  0.14502],
+                  [ 0.29451, -0.95464, -0.04397, -0.02296],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    tfms2.append(Thc.dot(tfms1[2]).dot(Tms))
+    
+    Tms_calib = solve_sylvester2(tfms1, tfms2)
+    
+    print "Tms: \n", Tms
+    print "Tms_calib: \n", Tms_calib
+    
+    R = Tms_calib[0:3,0:3]
+    print R.T.dot(R)
+    
+    assert (np.allclose(Tms_calib,Tms, atol=0.001))
+
+
+def test_tfm_val2():
+
+#     Tch = make_obs(0.5)
+#     Thc = nlg.inv(Tch)
+#     Tms = make_obs(0.1)
+#     I_0 = np.eye(4)
+#     I_0[3,3] = 0
+
+    tfms1 = []
+    tfms2 = []
+    
+    tfms1.append([[-0.0096,  -0.03728, -0.99926,  0.75043],
+                  [ 0.99995,  0.,      -0.0096,  -0.07002],
+                  [ 0.00036, -0.9993,   0.03728, -0.02286],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    tfms2.append([[ 0.2821,  -0.95904, -0.02587, -0.11373],
+                  [-0.70497, -0.18893, -0.68361,  0.03023],
+                  [ 0.65072,  0.21108, -0.72939,  0.00709],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    tfms1.append([[-0.0273,  -0.70346, -0.71021,  0.75183],
+                  [ 0.99952, -0.00897, -0.02954, -0.09279],
+                  [ 0.0144,  -0.71068,  0.70337, -0.088  ],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    tfms2.append([[ 0.09672, -0.98422,  0.14819, -0.14102],
+                  [-0.96374, -0.12981, -0.23314,  0.07651],
+                  [ 0.24869, -0.12027, -0.96109, -0.06604],
+                  [ 0.,       0.,       0.,       1.     ]])
+
+    tfms1.append([[-0.48571, -0.1099,  -0.86718,  0.65788],
+                  [ 0.82301,  0.27675, -0.49605,  0.14502],
+                  [ 0.29451, -0.95464, -0.04397, -0.02296],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    tfms2.append([[ 0.03962, -0.72105,  0.69175, -0.08271],
+                  [-0.62218, -0.55951, -0.54758, -0.18059],
+                  [ 0.78187, -0.4087,  -0.47079,  0.01134],
+                  [ 0.,       0.,       0.,       1.     ]])
+    
+    Tms_calib = solve_sylvester2(tfms1, tfms2)
+    
+    #print "Tms: \n", Tms
+    print "Tms_calib: \n", Tms_calib
+    
+    R = Tms_calib[0:3,0:3]
+    print R.T.dot(R)
+    
+    #assert (np.allclose(Tms_calib,Tms, atol=0.001))
