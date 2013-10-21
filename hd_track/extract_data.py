981c97dfa1302e7306d9b99a66af1ccdd667f5c1
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-10-20 21:43:39 -0700

    Added code to extract data from demos.

---
+++ hd_track/extract_data.py
@@ -0,0 +1,120 @@
+import numpy as np, numpy.linalg as nlg
+import rospy
+import os, os.path as osp
+import cPickle
+
+import roslib
+roslib.load_manifest('ar_track_service')
+from ar_track_service.srv import MarkerPositions, MarkerPositionsRequest, MarkerPositionsResponse
+from ar_track_alvar.msg import AlvarMarkers
+
+from hd_utils.defaults import tfm_link_rof
+from hd_utils import ros_utils as ru, clouds, conversions
+
+getMarkers = None
+req = MarkerPositionsRequest()
+
+def get_ar_marker_poses (pc):
+    global getMarkers, req
+    
+    if rospy.get_name() == '/unnamed':
+        rospy.init_node('keypoints')
+    
+    if getMarkers is None:
+        getMarkers = rospy.ServiceProxy("getMarkers", MarkerPositions)
+    
+    req.pc = pc
+    
+    marker_tfm = {}
+    res = getMarkers(req)
+    for marker in res.markers.markers:
+        marker_tfm[marker.id] = conversions.pose_to_hmat(marker.pose.pose).tolist()
+    
+    print "Marker ids found: ", marker_tfm.keys()
+    
+    return marker_tfm
+
+    
+def save_observations (bag, calib_file, save_file=None):
+    """
+    Assuming that the bag files have specific topics.
+    Also assuming the calib has only one gripper.
+    Can add more later.
+    """
+    
+    file_name = osp.join('/home/sibi/sandbox/human_demos/hd_data/calib',calib_file)
+    with open(file_name,'r') as fh: calib_data = cPickle.load(fh)
+    
+    c1_frame = 'camera1_link'
+    c2_frame = 'camera2_link'
+    hydra_frame = 'hydra_base'
+    tfm_c1_c2 = None
+    tfm_c1_h = None
+    
+    for tfm in calib_data['transforms']:
+        if tfm['parent'] == c1_frame or tfm['parent'] == '/' + c1_frame:
+            if tfm['child'] == c2_frame or tfm['child'] == '/' + c2_frame:
+                tfm_c1_c2 = nlg.inv(tfm_link_rof).dot(tfm['tfm']).dot(tfm_link_rof)
+            if tfm['child'] == hydra_frame or tfm['child'] == '/' + hydra_frame:
+                tfm_c1_h = nlg.inv(tfm_link_rof).dot(tfm['tfm'])
+    
+    if tfm_c1_c2 is None or tfm_c1_h is None:
+        redprint("Calibration does not have required transforms")
+        return
+    
+    if not calib_data.get('grippers'):
+        redprint("Gripper not found.")
+        return
+
+    lr,graph = calib_data['grippers'].items()[0]
+    gr = gripper.Gripper(lr, graph)
+    assert 'tool_tip' in gr.mmarkers
+    gr.tt_calculated = True
+
+    
+    ar1_tfms = []
+    ar2_tfms = []
+    hyd_tfms = []
+    
+    
+    for (topic, msg, _) in bag.read_messages(topics=['/camera1/depth_registered/points']):
+        marker_poses = get_ar_marker_poses (msg)
+        
+        if marker_poses:
+            tt_tfm = gr.get_tool_tip_transform(marker_poses, None)
+            ar1_tfms.append((tt_tfm, msg.header.stamp.to_secs()))
+        
+    for (topic, msg, _) in bag.read_messages(topics=['/camera2/depth_registered/points']):
+        marker_poses = get_ar_marker_poses (msg)
+        
+        if marker_poses:
+            for m in marker_poses:
+                marker_poses[m] = tfm_c1_c2.dot(marker_poses[m])
+            
+            tt_tfm = gr.get_tool_tip_transform(marker_poses, None)
+            ar2_tfms.append((tt_tfm, msg.header.stamp.to_secs()))
+
+    for (topic, msg, _) in bag.read_messages(topics=['/tf']):
+        
+        hyd_tfm = {}
+        for tfm in msg.transforms:
+            if tfm.frame_id == '/' + hydra_frame and tfm.child_frame_id == '/hydra_left':
+                t,r = tfm.transform.translation, tfm.transform.rotation
+                trans = (t.x,t.y,t.z)
+                rot = (t.x, t.y, t.z, t.w)
+                hyd_tfm['left'] = tfm_c1_h.dot(conversions.trans_rot_to_hmat(trans, rot))
+                break
+        
+        if hyd_tfm:
+            tt_tfm = gr.get_tool_tip_transform(hyd_tfm, None)
+            hyd_tfms.append((tt_tfm, msg.header.stamp.to_secs()))
+
+    if save_file is None:
+        save_file = ''
+        for name in bag.filename.split('.')[0:-1]:
+            save_file += name + '.'
+        save_file += 'data'
+    save_filename = osp.join('/home/sibi/sandbox/human_demos/hd_data/demos/obs_data', save_file)
+    
+    data = {'camera1':ar1_tfms, 'camera2':ar2_tfms, 'hydra':hyd_tfms}
+    with open(save_filename, 'w') as sfh: cPickle.dump(data, sfh)
