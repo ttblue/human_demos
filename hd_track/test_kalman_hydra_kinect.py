73e507242fd9d4aa3a21d64937b8ef012abc2473
Henry Lu <armentai@berkeley.edu>
2013-10-25 20:48:28 -0700

    merging

--- hd_track/test_kalman_hydra_kinect.py
+++ hd_track/test_kalman_hydra_kinect.py
@@ -39,13 +39,8 @@ def run_kalman_filter(T_hydra, T_ar, x_init, covar_init, ar_cov_scale, hydra_cov
     filter_estimates = [] ## the kalman filter estimates
     filter_covariances = [] ## the kalman filter covariances
     ## run the filter:
-<<<<<<< HEAD
-    for i in xrange(len(ts)-5):
-        KF.register_observation(ts[i], T_ar[i+1], T_hydra[i+1])
-=======
     for i in xrange(len(ts)-1):
         KF.register_observationx(ts[i], T_ar[i+1], T_hydra[i+1])
->>>>>>> 1df660059bdbe1ebe72731242160672ef7047b81
         filter_estimates.append(KF.x_filt)
         filter_covariances.append(KF.S_filt)
     
@@ -170,18 +165,9 @@ def load_data():
     Ts_bh = dat['Ts_bh']
     Ts_bg = dat['Ts_bg']
     Ts_ba = dat['Ts_ba']
-<<<<<<< HEAD
     T_gh = dat['T_gh']
     T_ga = dat['T_ga']
     
-
-    #Shifting the ar marker back three time stmps
-    Ts_ba = Ts_ba[3:]
-=======
-    T_gh  = dat['T_gh']
-    T_ga  = dat['T_ga']
->>>>>>> 1df660059bdbe1ebe72731242160672ef7047b81
-
     assert len(Ts_bg) == len(Ts_bh), "Number of hydra and pr2 transforms not equal."
     Ts_bh_hg = [t.dot(np.linalg.inv(T_gh)) for t in Ts_bh]
     Ts_ba_ag = []
