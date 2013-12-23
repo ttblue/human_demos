155c42684a2211d0c6c4693a6eb84b7d46431ce9
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-12-23 12:17:44 -0800

    make extract data work for multiple cameras

--- hd_track/extract_rgbd_data_demo.py
+++ hd_track/extract_rgbd_data_demo.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-dname", help="name of demonstration", action='store', dest='demo_name', type=str)
     parser.add_argument("-clib", help="name of calibration file", action='store', dest='calib_fname', type=str)
+    parser.add_argument("-cnum", help="number of cameras", action='store', dest="num_cameras", type=int, default=2)
     parser.add_argument("-sname", help="name of save file", action='store', dest='save_fname', default='', type=str)
     vals = parser.parse_args()
 
     calib_fname = osp.join(data_dir, 'calib', vals.calib_fname)
         
     if vals.save_fname == '':
-        ed.save_observations_rgbd(vals.demo_name, calib_fname)
+        ed.save_observations_rgbd(vals.demo_name, calib_fname, vals.num_cameras)
     else:
-        ed.save_observations_rgbd(vals.demo_name, calib_fname, vals.save_fname)
+        ed.save_observations_rgbd(vals.demo_name, calib_fname, vals.num_cameras, vals.save_fname)
