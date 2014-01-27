e91030317527a7440424ffc12203c2391a231e67
jia <panjia1983@gmail.com>
2014-01-26 17:36:32 -0800

    add non-interactive choice for perturbation

--- hd_utils/generate_perturbed_h5.py
+++ hd_utils/generate_perturbed_h5.py
@@ -15,6 +15,7 @@ parser.add_argument("--perturb_fname", help="File saved perturb point clouds")
 parser.add_argument("--perturb_num", help="Number of random perturbations", type=int, default=1)
 parser.add_argument("--max_perturb_attempt", help="Number of maximum attempts for perturbation", type=int, default=5)
 parser.add_argument("--overwrite", action="store_true")
+parser.add_argument("--interactive", action="store_true")
 
 args = parser.parse_args()
 
@@ -61,60 +62,81 @@ for demo in sorted(demofile.keys()):
     
     xyz = demofile[demo_name]['seg00']["cloud_xyz"]
     xyz = np.squeeze(xyz)
-    
-    mlab.figure(0)
-    mlab.clf()
-    mlab.points3d(xyz[:,0], xyz[:,1], xyz[:,2], color=(1,0,0), scale_factor=.005)
+    if has_hitch:
+        hitch_pos = demofile[demo_name]['seg00']['hitch_pos']
 
-    if yes_or_no("Do you want to add this original demo?"):
+
+    if args.interactive:
+        mlab.figure(0)
+        mlab.clf()
+        mlab.points3d(xyz[:,0], xyz[:,1], xyz[:,2], color=(1,0,0), scale_factor=.005)
+
+        if yes_or_no("Do you want to add this original demo?"):
+            perturb_name = str(n_perturb_existed)
+            perturb_group = demo_group.create_group(perturb_name)
+        
+            perturb_group['cloud_xyz'] = xyz
+            if has_hitch:
+                perturb_group['hitch_pos'] = np.squeeze(hitch_pos) 
+
+            print "add perturb demo %d"%(n_perturb_existed)
+            n_perturb_existed += 1
+        else:
+            pass       
+    
+        if yes_or_no("Do you want to skip perturbing this demo?"):
+            print "skip perturbing demo"
+            continue             
+    else:
         perturb_name = str(n_perturb_existed)
         perturb_group = demo_group.create_group(perturb_name)
-        
         perturb_group['cloud_xyz'] = xyz
-        
         if has_hitch:
-            hitch_pos = demofile[demo_name]['seg00']['hitch_pos']
             perturb_group['hitch_pos'] = np.squeeze(hitch_pos) 
-        
         print "add perturb demo %d"%(n_perturb_existed)
         n_perturb_existed += 1
-    else:
-        pass       
-    
-    if yes_or_no("Do you want to skip perturbing this demo?"):
-        print "skip perturbing demo"
-        continue             
-        
+
     # start actual perturbation    
     n_perturbed = 0
     n_perturbed_attempt = 0
-    
-        
+            
     while n_perturbed <= args.perturb_num and n_perturbed_attempt < args.max_perturb_attempt:
         
         if has_hitch:
-            new_object_xyz = sample_random_rope(demofile[demo_name]['seg00']['object'], True)
+            new_object_xyz = sample_random_rope(demofile[demo_name]['seg00']['object'], args.interactive)
             new_object_xyz = clouds.downsample(new_object_xyz, .01)
             new_xyz = np.r_[new_object_xyz, demofile[demo_name]['seg00']['hitch']]
         else:
-            new_xyz = sample_random_rope(xyz, True)
+            new_xyz = sample_random_rope(xyz, args.interactive)
             new_xyz = clouds.downsample(new_xyz, .01)
-
-        mlab.figure(0)
-        mlab.clf()
-        mlab.points3d(new_xyz[:,0], new_xyz[:,1], new_xyz[:,2], color=(1,0,0), scale_factor=.005)
-
-
-        if yes_or_no("Are you happy with this perturbation?"):
+            
+        if args.interactive:
+            mlab.figure(0)
+            mlab.clf()
+            mlab.points3d(new_xyz[:,0], new_xyz[:,1], new_xyz[:,2], color=(1,0,0), scale_factor=.005)
+    
+    
+            if yes_or_no("Are you happy with this perturbation?"):
+                perturb_name = str(n_perturb_existed)
+                perturb_group = demo_group.create_group(perturb_name)
+                perturb_group['cloud_xyz'] = new_xyz
+                print "add perturb demo %d"%(n_perturb_existed)
+                n_perturbed += 1
+                n_perturb_existed += 1
+          
+            n_perturbed_attempt += 1
+        else:
             perturb_name = str(n_perturb_existed)
             perturb_group = demo_group.create_group(perturb_name)
             perturb_group['cloud_xyz'] = new_xyz
             print "add perturb demo %d"%(n_perturb_existed)
             n_perturbed += 1
             n_perturb_existed += 1
-      
-        n_perturbed_attempt += 1      
+            n_perturbed_attempt += 1
+            
+            
+                  
 
-    
-    if yes_or_no("Stop perturbation?"):
-        break        
+    if args.interactive:
+        if yes_or_no("Stop perturbation?"):
+            break        
