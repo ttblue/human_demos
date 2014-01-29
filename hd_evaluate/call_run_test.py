2c889ea0f98fc94987fab7f0ae586f46527116ed
Ankush Gupta <ankush.me@hotmail.com>
2014-01-29 04:46:12 -0800

    call run_test_cloud as a function for picloud:

---
+++ hd_evaluate/call_run_test.py
@@ -0,0 +1,67 @@
+## script to call hd_playback/run_test.py : for use on pycloud
+
+from hd_playback import run_test
+
+
+class SimArgs:
+    use_diff_length = True
+    cloud_proc_func = "extract_red"
+    cloud_proc_mod  = "hd_utils.cloud_proc_funcs"
+    execution  = 0
+    animation  = 0
+    simulation = 1
+    parallel   = 3
+    downsample = 1
+    prompt     = False
+    show_neighbors = False
+    log    = False
+    fake_data_transform = (0,0,0,0,0,0)
+    trajopt_init = "openrave_ik"
+    interactive = False
+    remove_table = False
+    
+    use_ar_init = True
+    ar_demo_file = ""
+    ar_run_file  = ""
+    
+    use_base = False
+    not_allow_base = False
+    early_stop_portion = 0.5
+    no_traj_resample = False
+    
+
+    closest_rope_hack = False
+    closest_rope_hack_thresh = 0.01
+    
+    pot_threshold = 15
+    select = "clusters"
+    friction = 30.0   
+    max_steps_before_failure = 5
+    tps_bend_cost_init  = 10
+    tps_bend_cost_final = 0.1
+    tps_n_inter         = 50
+    cloud_downsample    = 0.02
+    
+    ndemos              = -1
+    rope_scaling_factor = 1.0
+    state_save_fname  = ''
+    demo_type         = ''
+    demo_data_h5_prefix = ''
+    init_state_h5     = ''
+    init_demo_name    = ''
+    init_perturb_name = ''
+    state_save_fname  = ''
+
+
+
+def run_sim_test(cmdline_params):
+    SimArgs.ndemos              = cmdline_params[0]
+    SimArgs.demo_type           = cmdline_params[1]
+    SimArgs.demo_data_h5_prefix = cmdline_params[2]
+    SimArgs.init_state_h5       = cmdline_params[3]
+    SimArgs.init_demo_name      = cmdline_params[4]
+    SimArgs.init_perturb_name   = cmdline_params[5]
+    SimArgs.rope_scaling_factor = cmdline_params[6]
+    SimArgs.state_save_fname    = cmdline_params[7]
+    
+    return run_test.main(SimArgs)
